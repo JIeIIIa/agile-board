8d0ee815a8b73da3939d7ced77a50b1117145f86
JIeIIIa <stealth@bigmir.net>
2019-02-12 23:31:29 +0200

    Add the backend module

---
+++ backend/src/main/java/com/gmail/onishchenko/oleksii/agile/AgileBoardApplication.java
@@ -0,0 +1,14 @@
+package com.gmail.onishchenko.oleksii.agile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileBoardApplication.class, args);
+	}
+
+}
+
---
+++ backend/src/test/java/com/gmail/onishchenko/oleksii/agile/AgileBoardApplicationTests.java
@@ -0,0 +1,17 @@
+package com.gmail.onishchenko.oleksii.agile;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AgileBoardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
