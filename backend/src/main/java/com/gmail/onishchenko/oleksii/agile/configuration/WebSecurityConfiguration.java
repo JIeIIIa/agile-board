4659e838a509855e982f4d4d037805cec484a1f7
JIeIIIa <stealth@bigmir.net>
2019-02-22 12:52:51 +0200

    RELEASE.1.0 Bug fixes

--- backend/src/main/java/com/gmail/onishchenko/oleksii/agile/configuration/WebSecurityConfiguration.java
+++ backend/src/main/java/com/gmail/onishchenko/oleksii/agile/configuration/WebSecurityConfiguration.java
@@ -51,7 +51,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "/index").permitAll()
+                .antMatchers("/", "/index*").permitAll()
                 .antMatchers("/manifest.json", "/service-worker.js", "/precache-manifest*").permitAll()
                 .antMatchers("/img/**").permitAll()
                 .antMatchers(HttpMethod.POST, "/api/login", "/api/registration").permitAll()
